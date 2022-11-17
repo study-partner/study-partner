ae2b3c5009b1ecf324a5b9b00d5ff8433bfcfa21
James Louie Grande <jlgrande@hawaii.edu>
2022-11-16 18:05:44 -1000

    Change landing page colors

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -5,7 +5,7 @@ import { PageIDs } from '../utilities/ids';
 /* A simple static component to render some text for the landing page. */
 const Landing = () => (
   <div id={PageIDs.landingPage}>
-    <div className="landing-green-background">
+    <div className="landing-color-background">
       <Container className="text-center">
         <h1 style={{ paddingTop: '20px', color: 'white', fontSize: '36pt' }}>
           Welcome to Bowfolios
@@ -17,7 +17,7 @@ const Landing = () => (
     </div>
     <div className="landing-white-background">
       <Container className="justify-content-center text-center">
-        <h2 style={{ color: '#376551' }}>Start by making your profile....</h2>
+        <h2 style={{ color: '#3C1874' }}>Start by making your profile....</h2>
         <Row md={1} lg={2}>
           <Col xs={6}>
             <Image src="/images/home-page.png" width={500} />
@@ -28,7 +28,7 @@ const Landing = () => (
         </Row>
       </Container>
     </div>
-    <div className="landing-green-background">
+    <div className="landing-color-background">
       <Container className="justify-content-center text-center">
         <h2 style={{ color: 'white' }}>...then add your projects</h2>
         <Row md={1} lg={2}>
@@ -42,7 +42,7 @@ const Landing = () => (
       </Container>
     </div>
     <div className="landing-white-background text-center">
-      <h2 style={{ color: '#376551' }}>
+      <h2 style={{ color: '#3C1874' }}>
         Connect to people and projects with shared interests!
       </h2>
       <Container>
