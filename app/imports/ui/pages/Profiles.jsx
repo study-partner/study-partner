6988303a9eebf75a7b4c6c12d347f0674d44e5b1
FeiyiChen01 <77761439+FeiyiChen01@users.noreply.github.com>
2022-11-28 14:25:01 -1000

    Revert "create"
    This reverts commit 5d71e571b6516574054bbb0b216c630d662497c0.

--- app/imports/ui/pages/Profiles.jsx
+++ app/imports/ui/pages/Profiles.jsx
@@ -12,7 +12,7 @@ import LoadingSpinner from '../components/LoadingSpinner';
 import { pageStyle } from './pageStyles';
 import { PageIDs } from '../utilities/ids';
 
-/* Returns the Profile and associated Session and NeedHelpClasses associated with the passed user email. */
+/* Returns the Profile and associated Projects and NeedHelpClasses associated with the passed user email. */
 function getProfileData(email) {
   const data = Profiles.collection.findOne({ email });
   const interests = _.pluck(ProfilesInterests.collection.find({ profile: email }).fetch(), 'interest');
