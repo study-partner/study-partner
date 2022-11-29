1017d9ee0b4c61923289d63076c1506d765f536d
ZianZengUH <zianzeng@hawaii.edu>
2022-11-29 11:48:15 -1000

    update calendar with demo events but not linked to database

--- app/imports/ui/pages/Calendar.jsx
+++ app/imports/ui/pages/Calendar.jsx
@@ -1,9 +1,137 @@
-import React from 'react';
-import { Scheduler } from '@aldabil/react-scheduler';
-import { EVENTS } from './events';
+import React, { Component } from 'react';
+import { DayPilot, DayPilotCalendar, DayPilotNavigator } from '@daypilot/daypilot-lite-react';
+import './CalendarStyles.css';
 
-const Calendar = () => (
-  <Scheduler events={EVENTS} />
-);
+const styles = {
+  wrap: {
+    display: 'flex',
+  },
+  left: {
+    marginRight: '10px',
+  },
+  main: {
+    flexGrow: '1',
+  },
+};
+
+class Calendar extends Component {
+
+  constructor(props) {
+    super(props);
+    this.calendarRef = React.createRef();
+    this.state = {
+      viewType: 'Week',
+      durationBarVisible: false,
+      timeRangeSelectedHandling: 'Enabled',
+      onTimeRangeSelected: async args => {
+        const dp = this.calendar;
+        const modal = await DayPilot.Modal.prompt('Create a new event:', 'Event 1');
+        dp.clearSelection();
+        if (!modal.result) { return; }
+        dp.events.add({
+          start: args.start,
+          end: args.end,
+          id: DayPilot.guid(),
+          text: modal.result,
+        });
+      },
+      eventDeleteHandling: 'Update',
+      onEventClick: async args => {
+        const dp = this.calendar;
+        const modal = await DayPilot.Modal.prompt('Update event text:', args.e.text());
+        if (!modal.result) { return; }
+        const e = args.e;
+        e.data.text = modal.result;
+        dp.events.update(e);
+      },
+    };
+  }
+
+  componentDidMount() {
+
+    const events = [
+      {
+        id: 1,
+        text: 'Event 1',
+        start: '2022-06-07T10:30:00',
+        end: '2022-06-07T13:00:00',
+      },
+      {
+        id: 2,
+        text: 'Event 2',
+        start: '2022-06-08T09:30:00',
+        end: '2022-06-08T11:30:00',
+        backColor: '#6aa84f',
+      },
+      {
+        id: 3,
+        text: 'Event 3',
+        start: '2022-06-08T09:30:00',
+        end: '2022-06-08T15:00:00',
+        backColor: '#f1c232',
+      },
+      {
+        id: 4,
+        text: 'Event 4',
+        start: '2022-06-06T11:30:00',
+        end: '2022-06-06T14:30:00',
+        backColor: '#cc4125',
+      },
+    ];
+
+    const date = new Date();
+
+    const day = date.getDate();
+    const month = date.getMonth() + 1;
+    const year = date.getFullYear();
+
+    // This arrangement can be altered based on how we want the date's format to appear.
+    const currentDate = `${year}-${month}-${day}`;
+
+    const startDate = currentDate;
+
+    this.calendar.update({ startDate, events });
+
+  }
+
+  get calendar() {
+    return this.calendarRef.current.control;
+  }
+
+  render() {
+    const date = new Date();
+
+    const day = date.getDate();
+    const month = date.getMonth() + 1;
+    const year = date.getFullYear();
+
+    // This arrangement can be altered based on how we want the date's format to appear.
+    const currentDate = `${year}-${month}-${day}`;
+    return (
+      <div style={styles.wrap}>
+        <div style={styles.left}>
+          <DayPilotNavigator
+            selectMode="week"
+            showMonths={3}
+            skipMonths={3}
+            startDate={currentDate}
+            selectionDay={currentDate}
+            onTimeRangeSelected={args => {
+              this.calendar.update({
+                startDate: args.day,
+              });
+            }}
+          />
+        </div>
+        <div style={styles.main}>
+          <DayPilotCalendar
+            {...this.state}
+            ref={this.calendarRef}
+          />
+        </div>
+      </div>
+    );
+  }
+}
 
 export default Calendar;
