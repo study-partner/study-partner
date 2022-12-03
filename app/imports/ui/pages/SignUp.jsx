fe84f01674617809edc59b48fe007f44912f957c
colbykag <59909940+colbykag@users.noreply.github.com>
2022-12-03 12:23:03 -1000

    Update SignUp.jsx

--- app/imports/ui/pages/SignUp.jsx
+++ app/imports/ui/pages/SignUp.jsx
@@ -33,7 +33,7 @@ const SignUp = () => {
       }
     });
     // Create the profile.
-    Profiles.collection.insert({ firstName: '', lastName: '', bio: '', title: '', picture: '', email });
+    Profiles.collection.insert({ firstName: '', lastName: '', bio: '', title: '', picture: '/images/study-partner-logo.png', email });
   };
 
   // if correct authentication, redirect to from: page instead of signup screen
